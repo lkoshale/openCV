b7a8c5c839749a0f9147f6d410cc32a0a7cff4d5
lkoshale <lkoshale@gmail.com>
2018-06-18 21:08:58 +0530

    script modified and data added

--- ImageSegmentation/SVMclf.py
+++ ImageSegmentation/SVMclf.py
@@ -7,19 +7,44 @@
 
 import cv2
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+import sys
+
 
 #resize images and create train dataset
 
+#read data from csv
 
-NUM_OF_CAT = 2
+df = pd.read_csv('train.csv')
+#suffle the data
+df = df.sample(frac=1).reset_index(drop=True)
+# print(df.info())
+
+name_list = df.path.tolist()
+target_list = df.category.tolist()
+
+# trainX,testX,trainY,testY = train_test_split(name_list)
+
+NUM_OF_CAT = 6
 
 def categorise_data(name):
-    if(name=='cat'):
+    if name == 'bed' :
         return 1
-    elif name == 'dog':
+    elif name == 'food_packet':
         return 2
+    elif name == 'plants':
+        return 3
+    elif name == 'fish_tank':
+        return 4
+    elif name == 'shampoo':
+        return 5
+    elif name == 'bowl':
+        return 6
+    else :
+        sys.exit(0)
 
 
 def create_image_mat(path_lsit,width,height):
@@ -41,16 +66,26 @@ def create_taget_matrix(cat_list):
 
     return Y
 
-list_img = []
-for i in range(1,5):
-    name = 'data\image'+str(i)+".jpg"
-    list_img.append(name)
+test = True
 
-trainX = create_image_mat(list_img,200,200)
-trainY = create_taget_matrix(['cat','dog'])
+if test :
+    list_img = []
 
-clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.1)
+    for i in name_list:
+        name = 'data\\'+str(i)
+        list_img.append(name)
 
+    X = create_image_mat(list_img,200,200)
+    Y = create_taget_matrix(target_list)
 
+    trainX,testX,trainY,testY = train_test_split(X,Y,random_state=42)
 
-print(trainY)
+    print(trainX.shape)
+    print(trainY.shape)
+
+    clf = GradientBoostingClassifier(n_estimators=100,learning_rate=0.1,verbose=True)
+    clf.fit(trainX,trainY)
+
+    print(clf.score(testX,testY))
+
+    # print(trainY)
---
+++ ImageSegmentation/create_train_csv.py
@@ -0,0 +1,8 @@
+
+# print("path,bed")
+
+generateFiles = False
+
+if generateFiles:
+    for i in range(1,60):
+        print("bowl ({}).jpg,bowl".format(i))
